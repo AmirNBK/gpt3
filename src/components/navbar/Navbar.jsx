d3eff5e2c4fcddc6ae882b3cfc6b4264827b3a6d
AmirNBK <80398198+AmirNBK@users.noreply.github.com>
2023-03-25 04:29:28 +0430

    developed header completely in all sizes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
 import logo from '../../Assets/logo.svg'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false)
 
     const linkItems = ['Home', 'What is GPT?', 'Open AI', 'Case Studies', 'Library']
+
+    if (window.innerWidth < 700) {
+        linkItems.push('Sign in', 'Sign up')
+    }
+
     return (
         <div className='Navbar'>
             <div className='Navbar__links'>
@@ -21,6 +28,22 @@ const Navbar = () => {
             <div className='Navbar__sign'>
                 <div className='Navbar__sign__signIn'> Sign in </div>
                 <button className='Navbar__sign__signUp'> Sign up </button>
+                <div className='Navbar__sign__menu'>
+                    {toggle ? <RiCloseLine onClick={() => { setToggle(false) }} size={27} /> :
+                        <RiMenu3Line onClick={() => { setToggle(true) }} size={27} />}
+                </div>
+                {toggle &&
+                    <div className='Navbar__sign__menu__container scale-up-center'>
+                        {linkItems.map((item) => {
+                            return (
+                                <div className='Navbar__sign__menu__container__items'>
+                                    {item}
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
+
             </div>
         </div>
     );
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Header.css'
+import ai from '../../Assets/ai.png'
+import people from '../../Assets/people.png'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='Header'>
+            <div className='Header__leftSide'>
+                <div className='Header__leftSide__title'> Let’s Build Something
+                    amazing with GPT-3
+                    OpenAI </div>
+
+                <p className='Header__leftSide__description'>
+                    Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+                </p>
+
+                <div className='Header__leftSide__Email'>
+                    <input type="email" placeholder='Your Email Address' className='Header__leftSide__Email__input' />
+                    <button className='Header__leftSide__Email__button'> Get Started </button>
+                </div>
+
+                <div className='Header__leftSide__people'>
+                    <div className='Header__leftSide__people__icons'> <img src={people} /> </div>
+                    <div className='Header__leftSide__people__description'> 1,600 people requested access a visit in last 24 hours </div>
+                </div>
+
+            </div>
+
+            <div className='Header__rightSide'>
+                <img src={ai} className='Header__rightSide__ai' />
+            </div>
         </div>
     );
 };
