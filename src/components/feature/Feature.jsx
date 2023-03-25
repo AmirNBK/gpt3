28ee29c21702da07fc214b56e94a9622c8f7715e
AmirNBK <80398198+AmirNBK@users.noreply.github.com>
2023-03-26 03:23:48 +0430

    developed the feature section in desktop size

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import FeaturesSectionItem from '../../containers/whatGPT3/featuresSectionItem/FeaturesSectionItem';
 import './Feature.css'
 
 const Feature = () => {
+
+    const features = [
+        { title: 'Improving end distrusts instantly', description: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.' },
+        { title: 'Become the tended active', description: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.' },
+        { title: 'Message or am nothing', description: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.' },
+        { title: 'Really boy law county', description: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.' }
+    ]
+
     return (
-        <div>
-            
+        <div className='Feature'>
+            <div className='Feature__leftSection'>
+                <h1 className='Feature__leftSection__description'> The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen. </h1>
+                <h1 className='Feature__leftSection__earlyAccess'> Request Early Access to Get Started </h1>
+            </div>
+
+            <div className='Feature__rightSection'>
+                {features.map((item) => {
+                    return (
+                        <div className='Feature__rightSection__container'>
+                            <FeaturesSectionItem title={item.title} description={item.description} />
+                        </div>
+                    )
+                })}
+            </div>
+
+
         </div>
     );
 };
--- src/containers/whatGPT3/featuresSectionItem/FeaturesSectionItem.jsx
+++ src/containers/whatGPT3/featuresSectionItem/FeaturesSectionItem.jsx
@@ -4,11 +4,13 @@ import './FeaturesSectionItem.css'
 const FeaturesSectionItem = (props) => {
     return (
         <div className='FeaturesSectionItem'>
-            <div className='FeaturesSectionItem__title'>
-                <div className='FeaturesSectionItem__title__line'>  </div>
-                <h1 className='FeaturesSectionItem__title__text'> {props.title} </h1>
+            <div className='FeaturesSectionItem__line'>  </div>
+            <div className='FeaturesSectionItem__content'>
+                <div className='FeaturesSectionItem__content__title'>
+                    <h1 className='FeaturesSectionItem__content__title__text'> {props.title} </h1>
+                </div>
+                <p className='FeaturesSectionItem__content__description'> {props.description} </p>
             </div>
-            <p className='FeaturesSectionItem__description'> {props.description} </p>
         </div>
     );
 };
