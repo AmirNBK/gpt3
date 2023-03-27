62e455456fd76e79ba3bd0bd2ee844757eb967bf
AmirNBK <80398198+AmirNBK@users.noreply.github.com>
2023-03-28 02:08:39 +0430

    developed CTA section in all sizes

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,16 @@ import './CTA.css'
 
 const CTA = () => {
     return (
-        <div>
-            
+        <div className='CTA'>
+            <div className='CTA__container'>
+                <div className='CTA__container__leftSection'>
+                    <h1> Request Early Access to Get Started </h1>
+                    <h1> Register today & start exploring the endless possiblities. </h1>
+                </div>
+                <div className='CTA__container__rightSection'>
+                    <button className='CTA__container__rightSection__button'> Get Started </button>
+                </div>
+            </div>
         </div>
     );
 };
