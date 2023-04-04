4e2ac26da8a880dbdca4e6caf0ae68b645499339
AmirNBK <80398198+AmirNBK@users.noreply.github.com>
2023-04-05 00:23:44 +0430

    developed footer on desktop

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,20 +3,20 @@ import './Footer.css'
 import logo from '../../Assets/logo.svg'
 
 const Footer = () => {
-    const firstFooterOptions = ['Links' , 'Overons' , 'Social Media' , 'Counters' , 'Counters']
-    const secondFooterOptions = ['Company' , 'Terms & Conditions' , 'Privacy Policy' , 'Contact']
-    const thirdFooterOptions = ['Get in touch' , 'Crechterwoord K12 182 DK Alknjkcb' , '085-132567' , 'info@payme.net']
+    const firstFooterOptions = ['Links', 'Overons', 'Social Media', 'Counters', 'Counters']
+    const secondFooterOptions = ['Company', 'Terms & Conditions', 'Privacy Policy', 'Contact']
+    const thirdFooterOptions = ['Get in touch', 'Crechterwoord K12 182 DK Alknjkcb', '085-132567', 'info@payme.net']
 
     return (
         <div className='Footer'>
             <div className='Footer__upperSection'>
-                 <h1> Do you want to step in to the future before others </h1>
-                 <button> Request Early Access </button>
+                <h1> Do you want to step in to the future before others </h1>
+                <button> Request Early Access </button>
             </div>
 
             <div className='Footer__bottomSection'>
                 <div className='Footer__bottomSection__logoSection'>
-                    <img src={logo}/>
+                    <img src={logo} />
                     <h1> Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved </h1>
                 </div>
                 <div className='Footer__bottomSection__firstOptions'>
@@ -34,15 +34,17 @@ const Footer = () => {
                     })}
                 </div>
                 <div>
-                <div className='Footer__bottomSection__thirdOptions'>
-                    {thirdFooterOptions.map((item) => {
-                        return (
-                            <h1> {item} </h1>
-                        )
-                    })}
-                </div>
+                    <div className='Footer__bottomSection__thirdOptions'>
+                        {thirdFooterOptions.map((item) => {
+                            return (
+                                <h1> {item} </h1>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
+
+            <h1 className='Footer__copyright'> © 2021 GPT-3. All rights reserved. </h1>
         </div>
     );
 };
